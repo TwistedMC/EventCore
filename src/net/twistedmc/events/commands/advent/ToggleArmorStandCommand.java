abfd7ea87afa6e986e0b96fc5de2cca9a01662c8
shastagrande <shastaofficial@gmail.com>
2021-11-17 15:04:12 -0500

    ?

--- src/net/twistedmc/events/commands/advent/ToggleArmorStandCommand.java
+++ src/net/twistedmc/events/commands/advent/ToggleArmorStandCommand.java
@@ -6,7 +6,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-<<<<<<< HEAD
 import twistedmc.core.util.api.API;
 
 public class ToggleArmorStandCommand implements CommandExecutor {
@@ -25,31 +24,11 @@ public class ToggleArmorStandCommand implements CommandExecutor {
                 return false;
             }
 
-            if (!API.systemDisabled("halloween")) {
+            if (!API.systemDisabled("adventCalendarNpc")) {
                 sender.sendMessage(c.green + "Disabled adventCalendarNpc: " + c.yellow + "Advent Calendar Figure");
                 API.disableSystem("adventCalendarNpc");
                 return false;
-=======
-
-public class ToggleArmorStandCommand implements CommandExecutor {
-    @Override
-    public boolean onCommand(CommandSender commandSender,Command cmd,String label,String[] args) {
-        if (cmd.getName().equalsIgnoreCase("toggleacas")) {
-            Player p = (Player) commandSender;
-            if (!p.hasPermission("rank.owner")) {
-                commandSender.sendMessage(c.red + "I'm sorry, but you do not have permission to do that.");
-                return false;
             }
-
-            if (Main.ACArmorStandEnabled == true) {
-                Main.ACArmorStandEnabled = false;
-                p.sendMessage(c.red + "Disabled the ACArmorStand's click detection.");
-            } else {
-                Main.ACArmorStandEnabled = true;
-                p.sendMessage(c.green + "Enabled the ACArmorStand's click detection.");
->>>>>>> origin/master
-            }
-
         }
         return false;
     }
