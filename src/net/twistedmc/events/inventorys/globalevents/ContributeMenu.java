e2802711874cb26ecf60671e2915586697cdc3a2
MCEpicNation <30642012+MCEpicNation@users.noreply.github.com>
2021-11-17 17:34:07 -0500

    Create ContributeMenu.java

---
+++ src/net/twistedmc/events/inventorys/globalevents/ContributeMenu.java
@@ -0,0 +1,34 @@
+package net.twistedmc.events.inventorys.globalevents;
+
+import me.arcaniax.hdb.api.HeadDatabaseAPI;
+import net.twistedmc.events.data.c;
+import net.twistedmc.events.util.item.AbstractGUI;
+import net.twistedmc.events.util.item.cItemStack;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+
+public class ContributeMenu extends AbstractGUI implements Listener {
+    HeadDatabaseAPI HEADapi = new HeadDatabaseAPI();
+    private static final int BORDER_FILL[] = {0,1,2,3,4,5,6,7,8,9,17,18,26,27,35,36,45,44,46,47,48,50,51,52,53};
+    /*
+    00 01 02 03 04 05 06 07 08
+    09 10 11 12 13 14 15 16 17
+    18 19 20 21 22 23 24 25 26
+    27 28 29 30 31 32 33 34 35
+    36 37 38 39 40 41 42 43 44
+    45 46 47 48    50 51 52 53
+     */
+    public ContributeMenu(Player player) {
+        super(6,"Contribute Snowflakes",player);
+        setItem(new cItemStack(Material.ARROW).setDisplayName(c.green + "Close").addLore
+                        (c.gray + "Close this menu")
+                , 49, (s,c,p) -> { });
+    }
+
+
+  //  @EventHandler(priority = EventPriority.NORMAL,ignoreCancelled = true)
+
+}
